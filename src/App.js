1a8fb34fd381b95dd6b6a2f779b8a73eba8516a4
Tino <tinomuzambi@gmail.com>
2021-01-09 22:21:05 +0200

    Adds globe.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Globe from "react-globe.gl";
 
 function App() {
-	return "hi";
+	return (
+		<Globe
+			globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+			bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+			backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+		/>
+	);
 }
 
 export default App;
