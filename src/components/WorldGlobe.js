5c31a9b56ea162f3b116e87563f061e641a6ffed
Tino <tinomuzambi@gmail.com>
2021-01-09 23:22:58 +0200

    Adds background image.

--- src/components/WorldGlobe.js
+++ src/components/WorldGlobe.js
@@ -3,10 +3,11 @@ import Globe from "react-globe.gl";
 
 const WorldGlobe = () => {
 	const globeEl = useRef();
-	const points = [
-		{ lat: "-25.668501", lng: "27.242371" },
-		{ lat: "-33.92487", lng: "18.424055" },
-	];
+	// const points = [
+	// 	{ lat: "-25.668501", lng: "27.242371" },
+	// 	{ lat: "-33.92487", lng: "18.424055" },
+	// 	{ lat: "-17.825167", lng: "31.033510" },
+	// ];
 
 	useEffect(() => {
 		// Auto-rotate
@@ -15,14 +16,19 @@ const WorldGlobe = () => {
 	}, []);
 
 	return (
-		<Globe
-			ref={globeEl}
-			hexBinPointsData={points}
-			hexAltitude={1}
-			globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
-			bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-			backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-		/>
+		<div className="globe-wrapper">
+			<img src="assets/sky.png" alt="sky" className="sky" />
+			<div className="globe">
+				<Globe
+					ref={globeEl}
+					width={500}
+					height={400}
+					globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+					bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+					backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+				/>
+			</div>
+		</div>
 	);
 };
 
