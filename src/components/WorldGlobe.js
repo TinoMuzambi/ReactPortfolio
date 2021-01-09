56a7a0fbf9f2811d02ddf47cc2b507562a928524
Tino <tinomuzambi@gmail.com>
2021-01-09 22:46:44 +0200

    Adds globe rotation and labels.

--- src/components/WorldGlobe.js
+++ src/components/WorldGlobe.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import Globe from "react-globe.gl";
 
 const WorldGlobe = () => {
+	const globeEl = useRef();
+	const points = [
+		{ lat: "-25.668501", lng: "27.242371" },
+		{ lat: "-33.92487", lng: "18.424055" },
+	];
+
+	useEffect(() => {
+		// Auto-rotate
+		globeEl.current.controls().autoRotate = true;
+		globeEl.current.controls().autoRotateSpeed = 0.2;
+	}, []);
+
 	return (
 		<Globe
-			className="globe"
+			ref={globeEl}
+			hexBinPointsData={points}
+			hexAltitude={1}
 			globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
 			bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
 			backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
