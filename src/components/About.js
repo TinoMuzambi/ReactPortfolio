29a2d9e8530979d3190ac465a30cacd72f1fc1a6
Tino <tinomuzambi@gmail.com>
2021-01-10 11:17:59 +0200

    Adds cards and images for cards.

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,8 @@ const About = () => {
 		<div className="about">
 			<h1 className="title">About</h1>
 			<div className="mini-card">
+				<img src="/assets/about.png" alt="person" className="tag" />
+				{/* Icons made by <a href="https://www.flaticon.com/authors/payungkead" title="Payungkead">Payungkead</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a> */}
 				<p className="text">
 					I'm Tino. An avid techie and software developer based in Cape Town,
 					South Africa. I'm keen on creating software that solves the problems
@@ -13,6 +15,18 @@ const About = () => {
 				</p>
 			</div>
 			<div className="mini-card">
+				<img src="/assets/hobbies.png" alt="person" className="tag" />
+
+				<p className="text">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum odit
+					aspernatur consequuntur voluptates ipsa earum, et, laboriosam aliquam,
+					architecto dignissimos impedit doloremque quaerat reiciendis fugit
+					assumenda! Tempore animi incidunt perferendis!
+				</p>
+			</div>
+			<div className="mini-card">
+				<img src="/assets/socials.png" alt="person" className="tag" />
+
 				<p className="text">
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum odit
 					aspernatur consequuntur voluptates ipsa earum, et, laboriosam aliquam,
