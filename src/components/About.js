62c34f619f8614df71fe11b6d47aa77bc4534dec
Tino <tinomuzambi@gmail.com>
2021-01-10 10:40:13 +0200

    Styles about content card.

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,14 @@ const About = () => {
 	return (
 		<div className="about">
 			<h1 className="title">About</h1>
+			<div className="me">
+				<p className="text">
+					I'm Tino. An avid techie and software developer based in Cape Town,
+					South Africa. I'm keen on creating software that solves the problems
+					that everyday people have. I have a strong foundation in Computer
+					Science and am always looking to learn new things.
+				</p>
+			</div>
 		</div>
 	);
 };
