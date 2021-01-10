988146671de1b7efcc1567a84810a7b39e0ca3ce
Tino <tinomuzambi@gmail.com>
2021-01-10 14:32:39 +0200

    Adds portfolio item cards.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,43 @@
 import React from "react";
+import projects from "../data/projects";
 
 const Portfolio = () => {
 	return (
 		<div className="portfolio">
 			<h1 className="title">Portfolio</h1>
+
+			<div className="cards">
+				{projects.splice(0, 5).map((project, key) => (
+					<div className="mini-card">
+						<h2 className="title">
+							{project.link ? (
+								<a
+									target="_blank"
+									rel="noopener noreferrer"
+									href={project.link}
+								>
+									{project.title}
+								</a>
+							) : (
+								project.title
+							)}
+						</h2>
+						<p className="text">{project.content[0]}</p>
+
+						{project.github && (
+							<p className="text">
+								<a
+									target="_blank"
+									rel="noopener noreferrer"
+									href={project.github}
+								>
+									GitHub
+								</a>
+							</p>
+						)}
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
