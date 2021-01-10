8143a5d2a166d6063acd3ef6c91b323b646f0fbb
Tino <tinomuzambi@gmail.com>
2021-01-10 22:08:13 +0200

    Adds preloader.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { useEffect } from "react";
 import "./css/App.min.css";
 import Globe from "./components/WorldGlobe";
 import Hero from "./components/Hero";
 import Holder from "./components/Holder";
+import Preload from "./pages/Preload";
 
 function App() {
+	useEffect(() => {
+		window.addEventListener("load", () => {
+			// Get rid of preloader once everything's loaded
+			const preload = document.querySelector(".preload");
+
+			preload.classList.add("preload-finish");
+		});
+
+		return () => {
+			window.removeEventListener("load", () => {
+				const preload = document.querySelector(".preload");
+
+				preload.classList.add("preload-finish");
+			});
+		};
+	}, []);
+
 	return (
 		<>
+			<Preload /> {/* Preloader for showing before page loads. */}
 			<Globe />
 			<Hero />
 			<Holder />
