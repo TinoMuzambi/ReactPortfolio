5a2a09ce38185e3b61d1f506658ee090f9707d97
Tino <tinomuzambi@gmail.com>
2021-01-10 11:01:54 +0200

    Adds about mini card class.

--- src/components/About.js
+++ src/components/About.js
@@ -4,12 +4,20 @@ const About = () => {
 	return (
 		<div className="about">
 			<h1 className="title">About</h1>
-			<div className="me">
+			<div className="mini-card">
 				<p className="text">
 					I'm Tino. An avid techie and software developer based in Cape Town,
 					South Africa. I'm keen on creating software that solves the problems
 					that everyday people have. I have a strong foundation in Computer
 					Science and am always looking to learn new things.
+				</p>
+			</div>
+			<div className="mini-card">
+				<p className="text">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum odit
+					aspernatur consequuntur voluptates ipsa earum, et, laboriosam aliquam,
+					architecto dignissimos impedit doloremque quaerat reiciendis fugit
+					assumenda! Tempore animi incidunt perferendis!
 				</p>
 			</div>
 		</div>
