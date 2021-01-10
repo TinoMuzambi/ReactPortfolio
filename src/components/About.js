e01f350e7da888d23c6a4be2ae7320ecb278ec74
Tino <tinomuzambi@gmail.com>
2021-01-10 18:23:18 +0200

    Adds heading to mini about cards,

--- src/components/About.js
+++ src/components/About.js
@@ -13,86 +13,95 @@ const About = () => {
 		<div className="about">
 			<h1 className="title">About</h1>
 			<div className="mini-card">
-				<img src="/assets/about.png" alt="person" className="tag" />
-				{/* Icons made by <a href="https://www.flaticon.com/authors/payungkead" title="Payungkead">Payungkead</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a> */}
-				<p className="text">
-					I'm Tino. An avid techie and software developer based in Cape Town,
-					South Africa. I'm keen on creating software that solves the problems
-					that everyday people have. I have a strong foundation in Computer
-					Science and am always looking to learn new things.
-				</p>
+				<h2 className="subtitle">Bio</h2>
+				<div className="inner">
+					<img src="/assets/about.png" alt="person" className="tag" />
+					{/* Icons made by <a href="https://www.flaticon.com/authors/payungkead" title="Payungkead">Payungkead</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a> */}
+					<p className="text">
+						I'm Tino. An avid techie and software developer based in Cape Town,
+						South Africa. I'm keen on creating software that solves the problems
+						that everyday people have. I have a strong foundation in Computer
+						Science and am always looking to learn new things.
+					</p>
+				</div>
 			</div>
 			<div className="mini-card">
-				<img src="/assets/hobbies.png" alt="person" className="tag" />
+				<h2 className="subtitle">Hobbies</h2>
+				<div className="inner">
+					<img src="/assets/hobbies.png" alt="person" className="tag" />
 
-				<p className="text">
-					In my free time I enjoy playing the piano, solving the Rubik's cube as
-					well as discovering new places. I'm also a hobby video editor, and a
-					hobby blogger. You can read more about both of those and about me in
-					general on my blog{" "}
-					<a
-						href="https://blog.tinomuzambi.com"
-						target="_blank"
-						rel="noreferrer"
-						className="blog-link"
-					>
-						blog.tinomuzambi.com
-					</a>
-					.
-				</p>
+					<p className="text">
+						In my free time I enjoy playing the piano, solving the Rubik's cube
+						as well as discovering new places. I'm also a hobby video editor,
+						and a hobby blogger. You can read more about both of those and about
+						me in general on my blog{" "}
+						<a
+							href="https://blog.tinomuzambi.com"
+							target="_blank"
+							rel="noreferrer"
+							className="blog-link"
+						>
+							blog.tinomuzambi.com
+						</a>
+						.
+					</p>
+				</div>
 			</div>
 			<div className="mini-card">
-				<img src="/assets/socials.png" alt="person" className="tag" />
+				<h2 className="subtitle">Socials</h2>
+				<div className="inner">
+					<img src="/assets/socials.png" alt="person" className="tag" />
 
-				<div className="socials">
-					<a
-						href="https://bit.ly/TinoFacebook"
-						className="link"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<FaFacebook className="icon" />
-					</a>
-					<a
-						href="https://bit.ly/TinoLinkedIn"
-						className="link"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<FaLinkedin className="icon" />
-					</a>
-					<a
-						href="https://bit.ly/TinoTwitter"
-						className="link"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<FaTwitter className="icon" />
-					</a>
-					<a
-						href="https://bit.ly/TinoInstagram"
-						className="link"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<FaInstagram className="icon" />
-					</a>
-					<a
-						href="https://bit.ly/TinoYouTube"
-						className="link"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<FaYoutube className="icon" />
-					</a>
-					<a
-						href="https://bit.ly/TinoGitHub"
-						className="link"
-						target="_blank"
-						rel="noreferrer"
-					>
-						<FaGithub className="icon" />
-					</a>
+					<div className="socials">
+						<a
+							href="https://bit.ly/TinoFacebook"
+							className="link"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FaFacebook className="icon" />
+						</a>
+						<a
+							href="https://bit.ly/TinoLinkedIn"
+							className="link"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FaLinkedin className="icon" />
+						</a>
+						<a
+							href="https://bit.ly/TinoTwitter"
+							className="link"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FaTwitter className="icon" />
+						</a>
+						<a
+							href="https://bit.ly/TinoInstagram"
+							className="link"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FaInstagram className="icon" />
+						</a>
+						<a
+							href="https://bit.ly/TinoYouTube"
+							className="link"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FaYoutube className="icon" />
+						</a>
+						<a
+							href="https://bit.ly/TinoGitHub"
+							className="link"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FaGithub className="icon" />
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
