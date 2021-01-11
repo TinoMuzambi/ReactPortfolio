82495204c9949c13de82774dde8e9ec85d8a0f41
Tino <tinomuzambi@gmail.com>
2021-01-11 17:41:24 +0200

    Adds table tennis to about. Also modifies gitignore.

--- src/components/About.js
+++ src/components/About.js
@@ -35,10 +35,10 @@ const About = () => {
 					<img src="/assets/hobbies.png" alt="person" className="tag" />
 
 					<p className="text">
-						In my free time I enjoy playing the piano, solving the Rubik's cube
-						as well as discovering new places. I'm also a hobby video editor,
-						and a hobby blogger. You can read more about both of those and about
-						me in general on my blog{" "}
+						In my free time I enjoy playing the piano, solving the Rubik's cube,
+						playing Table Tennis as well as discovering new places. I'm also a
+						hobby video editor, and a hobby blogger. You can read more about
+						both of those and about me in general on my blog{" "}
 						<a
 							href="https://blog.tinomuzambi.com"
 							target="_blank"
