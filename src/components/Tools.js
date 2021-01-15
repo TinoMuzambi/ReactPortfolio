5450ca4e95453fc20adf2c661aead3ed99eef692
Tino <tinomuzambi@gmail.com>
2021-01-15 19:38:31 +0200

    Adds fade in.

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,12 +1,15 @@
 import React from "react";
 import tools from "../data/tools";
+import { useSpring, animated } from "react-spring";
 
 const Tools = () => {
+	const props = useSpring({ opacity: 1, from: { opacity: 0 } });
+
 	return (
-		<div className="tools">
+		<animated.div className="tools" style={props}>
 			<h1 className="title">Tools and Technologies</h1>
 			<div className="content">{tools.map((tool) => tool.icon)}</div>
-		</div>
+		</animated.div>
 	);
 };
 
