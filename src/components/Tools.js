415920a24192340bf17b796b0a0f12bdd3f78854
Tino <tinomuzambi@gmail.com>
2021-01-15 18:27:48 +0200

    Adds title.

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -2,7 +2,12 @@ import React from "react";
 import tools from "../data/tools";
 
 const Tools = () => {
-	return <div className="tools">{tools.map((tool, key) => tool.icon)}</div>;
+	return (
+		<div className="tools">
+			<h1 className="title">Tools and Technologies</h1>
+			<div className="content">{tools.map((tool, key) => tool.icon)}</div>
+		</div>
+	);
 };
 
 export default Tools;
