bd22612a8a2f4904005febd724aae238d3c058a7
Tino <tinomuzambi@gmail.com>
2021-01-15 22:13:51 +0200

    Refactoring how tools work.

--- src/data/tools.js
+++ src/data/tools.js
@@ -2,318 +2,35 @@ import android from "./tools/android.png";
 import c from "./tools/c.png";
 import cpp from "./tools/c++.png";
 import css from "./tools/css.png";
+import firebase from "./tools/firebase.png";
+import flask from "./tools/flask.png";
 import gimp from "./tools/gimp.png";
 import git from "./tools/git.png";
 import github from "./tools/github.png";
-import godaddy from "./tools/godaddy.png";
 import golang from "./tools/golang.png";
-import google from "./tools/google.png";
 import heroku from "./tools/heroku.png";
 import hitfilm from "./tools/hitfilm.png";
 import html from "./tools/html.png";
 import java from "./tools/java.png";
+import javascript from "./tools/javascript.png";
 import jetbrains from "./tools/jetbrains.png";
 import mongodb from "./tools/mongodb.png";
 import netlify from "./tools/netlify.png";
 import nodejs from "./tools/nodejs.png";
-import notepadpp from "./tools/notepad++.png";
+import npm from "./tools/npm.png";
 import office from "./tools/office.png";
 import python from "./tools/python.png";
 import react from "./tools/react.png";
-import rstudio from "./tools/rstudio.png";
-import selenium from "./tools/selenium.png";
-import slack from "./tools/slack.png";
-import tableau from "./tools/tableau.png";
+import sass from "./tools/sass.png";
+import typescript from "./tools/typescript.png";
 import ubuntu from "./tools/ubuntu.png";
 import vercel from "./tools/vercel.png";
 import vs_code from "./tools/vs_code.png";
 import wakatime from "./tools/wakatime.png";
+import yarn from "./tools/yarn.png";
 
 const tools = [
-	{
-		icon: (
-			<img
-				src={android}
-				alt="tool"
-				className="tools-img"
-				title="android"
-				key={0}
-			/>
-		),
-	},
-	{ icon: <img src={c} alt="tool" className="tools-img" title="c" key={1} /> },
-	{
-		icon: (
-			<img src={cpp} alt="tool" className="tools-img" title="cpp" key={2} />
-		),
-	},
-	{
-		icon: (
-			<img src={css} alt="tool" className="tools-img" title="css" key={3} />
-		),
-	},
-	{
-		icon: (
-			<img src={gimp} alt="tool" className="tools-img" title="gimp" key={4} />
-		),
-	},
-	{
-		icon: (
-			<img src={git} alt="tool" className="tools-img" title="git" key={5} />
-		),
-	},
-	{
-		icon: (
-			<img
-				src={github}
-				alt="tool"
-				className="tools-img"
-				title="github"
-				key={6}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={godaddy}
-				alt="tool"
-				className="tools-img"
-				title="godaddy"
-				key={7}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={golang}
-				alt="tool"
-				className="tools-img"
-				title="golang"
-				key={8}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={google}
-				alt="tool"
-				className="tools-img"
-				title="google"
-				key={9}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={heroku}
-				alt="tool"
-				className="tools-img"
-				title="heroku"
-				key={10}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={hitfilm}
-				alt="tool"
-				className="tools-img"
-				title="hitfilm"
-				key={11}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img src={html} alt="tool" className="tools-img" title="html" key={12} />
-		),
-	},
-	{
-		icon: (
-			<img src={java} alt="tool" className="tools-img" title="java" key={13} />
-		),
-	},
-	{
-		icon: (
-			<img
-				src={jetbrains}
-				alt="tool"
-				className="tools-img"
-				title="jetbrains"
-				key={14}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={mongodb}
-				alt="tool"
-				className="tools-img"
-				title="mongodb"
-				key={15}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={netlify}
-				alt="tool"
-				className="tools-img"
-				title="netlify"
-				key={16}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={nodejs}
-				alt="tool"
-				className="tools-img"
-				title="nodejs"
-				key={17}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={notepadpp}
-				alt="tool"
-				className="tools-img"
-				title="notepadpp"
-				key={18}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={office}
-				alt="tool"
-				className="tools-img"
-				title="office"
-				key={19}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={python}
-				alt="tool"
-				className="tools-img"
-				title="python"
-				key={29}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={react}
-				alt="tool"
-				className="tools-img"
-				title="react"
-				key={21}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={rstudio}
-				alt="tool"
-				className="tools-img"
-				title="rstudio"
-				key={22}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={selenium}
-				alt="tool"
-				className="tools-img"
-				title="selenium"
-				key={23}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={slack}
-				alt="tool"
-				className="tools-img"
-				title="slack"
-				key={24}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={tableau}
-				alt="tool"
-				className="tools-img"
-				title="tableau"
-				key={25}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={ubuntu}
-				alt="tool"
-				className="tools-img"
-				title="ubuntu"
-				key={26}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={vercel}
-				alt="tool"
-				className="tools-img"
-				title="vercel"
-				key={27}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={vs_code}
-				alt="tool"
-				className="tools-img"
-				title="vs_code"
-				key={28}
-			/>
-		),
-	},
-	{
-		icon: (
-			<img
-				src={wakatime}
-				alt="tool"
-				className="tools-img"
-				title="wakatime"
-				key={29}
-			/>
-		),
-	},
+	
 ];
 
 export default tools;
