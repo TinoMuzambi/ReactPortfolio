3baf2f172f6a6e8239c44be28de68e25fcaab13a
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-10 01:37:51 +0200

    Implements pseudos for tools titles.

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -23,6 +23,7 @@ const Tools = () => {
 							rel="noreferrer"
 							className="tools-img"
 							key={tool.id}
+							data-tool={tool.title}
 							initial="start"
 							animate="end"
 							variants={tool.id % 2 === 0 ? up : down}
@@ -39,20 +40,20 @@ const Tools = () => {
 							/>
 						</motion.a>
 					) : (
-						<motion.img
-							src={tool.icon}
-							alt={tool.title}
-							data-tool={tool.title}
-							className="tools-img"
-							key={tool.id}
-							initial="start"
-							animate="end"
-							variants={tool.id % 2 === 0 ? up : down}
-							transition={{
-								ease: "easeInOut",
-								duration: 0.2,
-							}}
-						></motion.img>
+						<span className="tools-img" data-tool={tool.title}>
+							<img
+								src={tool.icon}
+								alt={tool.title}
+								key={tool.id}
+								initial="start"
+								animate="end"
+								variants={tool.id % 2 === 0 ? up : down}
+								transition={{
+									ease: "easeInOut",
+									duration: 0.2,
+								}}
+							></img>
+						</span>
 					)
 				)}
 			</div>
