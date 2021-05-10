fd566fa508f6c5a47243c20ff4cd6bed6d07ce6b
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-11 01:57:46 +0200

    Adds sass.

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import "../sass/App.scss";
+
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-	return null;
+	return <h1>Hello</h1>;
 }
