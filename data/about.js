40301313984af9feffc606fcf0073154195cc8b0
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-11 02:38:02 +0200

    Moves images to storyblok.

--- data/about.js
+++ data/about.js
@@ -1,7 +1,7 @@
 const about = [
 	{
 		title: "Bio",
-		image: "/assets/about.png",
+		image: "https://a.storyblok.com/f/105639/512x512/6f0283b3e6/about.png",
 		text: `I'm Tino. An avid techie and software developer based in Cape Town,
         South Africa. I'm keen on creating software that solves the problems
         that everyday people have. I have a strong foundation in Computer
@@ -9,7 +9,7 @@ const about = [
 	},
 	{
 		title: "Hobbies",
-		image: "/assets/hobbies.png",
+		image: "https://a.storyblok.com/f/105639/512x512/0fdffa11eb/hobbies.png",
 		text: `In my free time I enjoy playing the piano, solving the Rubik's cube,
         playing Table Tennis as well as discovering new places. I'm also a
         hobby video editor, and a hobby blogger. You can read more about
@@ -26,7 +26,7 @@ const about = [
 	},
 	{
 		title: "Socials",
-		image: "/assets/socials.png",
+		image: "https://a.storyblok.com/f/105639/512x512/3bf8aa8310/socials.png",
 	},
 ];
 
