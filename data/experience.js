0290a128e5efb34539576d48970f98fb6b8dfcef
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-11 02:09:28 +0200

    Moving images to storyblok.

--- data/experience.js
+++ data/experience.js
@@ -1,9 +1,3 @@
-import SSC from "./ssc.png";
-import SPSC from "./spsc.png";
-import ElleZeka from "./elle.png";
-import Electrum from "./electrum.png";
-import UCT from "./uct.png";
-
 const experience = [
 	{
 		title: "Chairperson of the Science Postgraduate Students' Council",
@@ -13,7 +7,13 @@ const experience = [
 		other executive members in the execution of their duties as well as to be
 		the voice of the postgraduate students in meetings with University
 		executive.`,
-		icon: <img src={SPSC} alt="spsc" className="timeline-image" />,
+		icon: (
+			<img
+				src="https://a.storyblok.com/f/105639/400x171/25f0b09efb/spsc.png"
+				alt="spsc"
+				className="timeline-image"
+			/>
+		),
 	},
 	{
 		title: "Full-Stack Web Developer",
@@ -22,7 +22,13 @@ const experience = [
 		description: `I work on implementing front-end as well as full-stack website solutions for business clients. 
 			This post involves web designs, web hosting as well as the implementation of the
 			websites.`,
-		icon: <img src={ElleZeka} alt="elle zeka" className="timeline-image" />,
+		icon: (
+			<img
+				src="https://a.storyblok.com/f/105639/1038x1458/bf7bf532ab/elle.png"
+				alt="elle zeka"
+				className="timeline-image"
+			/>
+		),
 	},
 	{
 		title: "IT & Resource Manager of the Science Students' Council",
@@ -31,7 +37,13 @@ const experience = [
 		description: `The IT & Resource Manager is responsible for managing all online platforms of the SSC, ensuring all the IT related media is current as well as attending any grievances with regards to 
 			science undergraduate computer labs.
 			`,
-		icon: <img src={SSC} alt="ssc" className="timeline-image" />,
+		icon: (
+			<img
+				src="https://a.storyblok.com/f/105639/565x565/58b0dde7d2/ssc.png"
+				alt="ssc"
+				className="timeline-image"
+			/>
+		),
 	},
 	{
 		title: "Computer Science Tutor",
@@ -41,7 +53,13 @@ const experience = [
 			for first & second year Computer Science
 			courses primarily in Python & Java.
 			`,
-		icon: <img src={UCT} alt="uct" className="timeline-image" />,
+		icon: (
+			<img
+				src="https://a.storyblok.com/f/105639/1549x1573/6a2484db10/uct.png"
+				alt="uct"
+				className="timeline-image"
+			/>
+		),
 	},
 	{
 		title: "Software Developer Intern",
@@ -49,7 +67,13 @@ const experience = [
 		period: "June 2019 - July 2019",
 		description: `I worked on a real life project that was implemented by Capitec Bank.
 		Technologies used include Java, XML and AWS.`,
-		icon: <img src={Electrum} alt="electrum" className="timeline-image" />,
+		icon: (
+			<img
+				src="https://a.storyblok.com/f/105639/261x103/e7eeeeddb4/electrum.png"
+				alt="electrum"
+				className="timeline-image"
+			/>
+		),
 	},
 ];
 
