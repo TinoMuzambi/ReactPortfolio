ada5261d0f16f422c68d3cd5b36fd011e3fce1d2
Tino Muzambi <tinomuzambi@gmail.com>
2021-12-11 07:07:41 +0200

    Makes form inputs controlled.

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -5,23 +5,47 @@ const ContactForm = () => {
 	const [email, setEmail] = useState("");
 	const [subject, setSubject] = useState("");
 	const [message, setMessage] = useState("");
+
 	return (
 		<form className="form">
 			<div className="input-group">
 				<label htmlFor="name">Name:</label>
-				<input type="text" name="name" id="name" />
+				<input
+					type="text"
+					name="name"
+					id="name"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+				/>
 			</div>
 			<div className="input-group">
 				<label htmlFor="email">Email:</label>
-				<input type="email" name="email" id="email" />
+				<input
+					type="email"
+					name="email"
+					id="email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
 			</div>
 			<div className="input-group">
 				<label htmlFor="subject">Subject:</label>
-				<input type="text" name="subject" id="subject" />
+				<input
+					type="text"
+					name="subject"
+					id="subject"
+					value={subject}
+					onChange={(e) => setSubject(e.target.value)}
+				/>
 			</div>
 			<div className="input-group">
 				<label htmlFor="message">Message:</label>
-				<textarea name="message" id="message" />
+				<textarea
+					name="message"
+					id="message"
+					value={message}
+					onChange={(e) => setMessage(e.target.value)}
+				/>
 			</div>
 		</form>
 	);
