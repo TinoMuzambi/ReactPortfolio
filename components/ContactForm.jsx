aafbad2b69b16e6662dfa7e1ffd0c783d25a8d7f
Tino Muzambi <tinomuzambi@gmail.com>
2021-12-11 06:52:08 +0200

    Adds form markup.

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,5 +1,33 @@
 const ContactForm = () => {
-	return <div>Contact Me</div>;
+	return (
+		<div>
+			<h2 className="subtitle">Contact Me</h2>
+			<div className="inner">
+				<img
+					src="https://a.storyblok.com/f/105639/512x512/0fdffa11eb/hobbies.png"
+					alt="contact"
+				/>
+				<form className="form">
+					<div className="input-group">
+						<label htmlFor="name">Name:</label>
+						<input type="text" name="name" id="name" />
+					</div>
+					<div className="input-group">
+						<label htmlFor="email">Email:</label>
+						<input type="email" name="email" id="email" />
+					</div>
+					<div className="input-group">
+						<label htmlFor="subject">Subject:</label>
+						<input type="text" name="subject" id="subject" />
+					</div>
+					<div className="input-group">
+						<label htmlFor="message">Message:</label>
+						<textarea name="message" id="message" />
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default ContactForm;
