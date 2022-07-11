600f99f49f6aff447d57dd61b192051c9d9c084c
Tino Muzambi <tinomuzambi@gmail.com>
2022-07-11 14:53:21 +0200

    Adds updated meta.

--- utils/index.js
+++ utils/index.js
@@ -89,3 +89,11 @@ export function shuffle(array) {
 
 	return array;
 }
+
+/**
+ * Appends a limit to the end of text and substrings to the specified limit.
+ * @param text The text to add an ellips to.
+ * @param limit The point at which the text is cut off.
+ * @returns A string cut off at the limit and with an ellips appended to the end.
+ */
+export const elipsise = (text, limit) => `${text.substring(0, limit - 7)}...`;
